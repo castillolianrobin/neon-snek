115c08d1f39cdc4d63cdb9bb55501e3906c13300
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-07-04 20:14:17 +0800

    Chore: Remove unnecessary pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,59 +1,18 @@
-import { RouterView, createRouter, createWebHistory } from 'vue-router'
-import LayoutDefault from '@/layout/layouts/LayoutDefault.vue'
-import HomeViewVue from '@/views/public/HomeView.vue';
-// Middleware
-// import authentication from '@/middlewares/authentication';
-import { executeMiddlewares } from '@/middlewares';
-// Extra Routes
-// import dashboardRoutes from './dashboardRoutes';
-import componentRoutes from './componentRoutes';
-import { usePageLoadStore } from '@/stores/pageLoadStore';
+import { createRouter, createWebHistory } from 'vue-router'
+import SnakeGame from '@/views/SnakeGameView.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: import('@/views/SnakeGameView.vue')  ,
+      component: SnakeGame  ,
       meta: { layout: 'Default' },
     },
-
-    // Component Kit Routes
-    // {
-    //   path: '/_component',
-    //   component: LayoutDefault,
-    //   children: [
-    //     ...componentRoutes.map((route)=>({
-    //       path: route.path.substring(1),
-    //       name: route.name,
-    //       component: route.component,
-    //       meta: { ...route.meta, layout: 'ComponentDashboard' },
-    //     })),
-    //   ],
-    // },
   ]
 });
 
-
-
-router.beforeEach((to, from, next)=> {
-  //Set page loader to true
-  const { setPageLoading } = usePageLoadStore()
-  setPageLoading(true);
-  
-  
-  if (!to.meta.middleware) return next();
-
-  // Run middlewares
-  const context = {from, next, router, to };
-  return executeMiddlewares(context);
-})
-
-router.afterEach(()=>{
-  //Set page loader to true
-  const { setPageLoading } = usePageLoadStore()
-  setPageLoading(false);
-});
 
 export default router
 
