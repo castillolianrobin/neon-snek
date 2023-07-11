7d56f4cc2868eae768a48826a5ea590ab35523f3
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-07-11 19:54:42 +0800

    Chore: Remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import LayoutHandler from './layout/LayoutHandler.vue';
-import { TransitionFade } from '@/components/transitions'
-import ThePageLoader from './components/ThePageLoader.vue';
 </script>
 
 <template>
-  <ThePageLoader></ThePageLoader>
-  <LayoutHandler>
-    <RouterView  v-slot="{ Component }" >
-      <TransitionFade>
-        <component :is="Component"></component>
-      </TransitionFade>
-    </RouterView>      
-  </LayoutHandler>
+  <RouterView />      
 </template>
--- src/middlewares/test.ts
+++
@@ -1,9 +0,0 @@
-import type { RouterMiddleWare } from './index';
-
-
-const test:RouterMiddleWare = (context) => {
-  console.log('sample');
-  return context.next();
-} 
-
-export default test;
