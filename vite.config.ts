f8dc9a05d58398999388375abd0e807e91f21cf0
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-08-11 20:14:16 +0800

    Chore: Configure pwa to work with icon

--- vite.config.ts
+++ vite.config.ts
@@ -11,40 +11,48 @@ export default defineConfig({
     vue(), 
     // PWA Config
     VitePWA({ 
-      registerType: 
-      'autoUpdate', 
+      devOptions: { enabled: true },
+      registerType: 'autoUpdate', 
       injectRegister: 'auto',
       workbox: {
         cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
       },
+      includeAssets: [
+        '/favicon.ico',
+        '/img/icons/android-chrome-192x192.png',
+        '/img/icons/android-chrome-512x512.png',
+      ],
       manifest: {
         name: 'Retro Snake',
-        short_name: 'retsnek',
+        short_name: 'snek2',
         description: 'Classic Snake game with retro neo theme',
         theme_color: '#ec4899',
         background_color: '#71717a',
         display: 'standalone',
+        orientation: 'portrait',
         icons: [
           {
-            src: 'img/icons/android-chrome-60x60.png',
-            size: '60x60',
-            type: 'image/png',
-          },
-          {
-            src: 'img/icons/android-chrome-192x192.png',
-            size: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: 'img/icons/android-chrome-192x192.png',
+            src: '/img/icons/android-chrome-192x192.png',
             size: '192x192',
             type: 'image/png',
           },
           {
-            src: 'img/icons/android-chrome-512x512.png',
+            src: '/img/icons/android-chrome-512x512.png',
             size: '512x512',
             type: 'image/png',
+          },
+          {
+            src: '/img/icons/android-chrome-512x512.png',
+            size: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/img/icons/android-chrome-maskable-512x512.png',
+            size: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       } 
